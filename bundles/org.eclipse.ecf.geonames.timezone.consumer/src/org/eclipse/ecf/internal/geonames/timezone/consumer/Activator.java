2d218c55228086579caa3b206ec5e9a8226662f9
slewis <slewis@composent.com>
2014-11-28 11:38:58 -0800

    Changed use of synchronous ITimezoneService interface to use ITimezoneServiceAsync in consumer/example.

--- bundles/org.eclipse.ecf.geonames.timezone.consumer/src/org/eclipse/ecf/internal/geonames/timezone/consumer/Activator.java
+++ bundles/org.eclipse.ecf.geonames.timezone.consumer/src/org/eclipse/ecf/internal/geonames/timezone/consumer/Activator.java
@@ -1,63 +1,87 @@
 package org.eclipse.ecf.internal.geonames.timezone.consumer;
 
-import org.geonames.timezone.ITimezoneService;
-import org.geonames.timezone.Timezone;
+import org.geonames.timezone.ITimezoneServiceAsync;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
-public class Activator implements BundleActivator, ServiceTrackerCustomizer<ITimezoneService, ITimezoneService> {
+public class Activator implements BundleActivator,
+		ServiceTrackerCustomizer<ITimezoneServiceAsync, ITimezoneServiceAsync> {
 
 	private static BundleContext context;
 
-	private ServiceTracker<ITimezoneService,ITimezoneService> tracker;
-	
+	private ServiceTracker<ITimezoneServiceAsync, ITimezoneServiceAsync> tracker;
+
 	static BundleContext getContext() {
 		return context;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		tracker = new ServiceTracker<ITimezoneService, ITimezoneService>(context,ITimezoneService.class,this);
+		tracker = new ServiceTracker<ITimezoneServiceAsync, ITimezoneServiceAsync>(
+				context, ITimezoneServiceAsync.class, this);
 		tracker.open();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
+		if (tracker != null) {
+			tracker.close();
+			tracker = null;
+		}
 		Activator.context = null;
 	}
 
 	@Override
-	public ITimezoneService addingService(
-			ServiceReference<ITimezoneService> reference) {
-		ITimezoneService service = getContext().getService(reference);
-		System.out.println("Got ITimezoneService");
-		Timezone timezone = service.getTimezone(47.01, 10.2);
-		System.out.println("Got timezone="+timezone);
+	public ITimezoneServiceAsync addingService(
+			ServiceReference<ITimezoneServiceAsync> reference) {
+		ITimezoneServiceAsync service = getContext().getService(reference);
+		System.out.println("Got ITimezoneServiceAsync");
+		// Get completable future and when complete
+		service.getTimezoneAsync(47.01, 10.2).whenComplete(
+				(result, exception) -> {
+					// Check for exception and print out
+					if (exception != null) {
+						System.out.println(exception.getMessage());
+						exception.printStackTrace();
+					} else
+						// Success!
+						System.out.println("Received response:  timezone="
+								+ result);
+				});
+		// Report
+		System.out.println("Returning ITimezoneServiceAsync");
 		return service;
 	}
 
 	@Override
-	public void modifiedService(ServiceReference<ITimezoneService> reference,
-			ITimezoneService service) {
+	public void modifiedService(
+			ServiceReference<ITimezoneServiceAsync> reference,
+			ITimezoneServiceAsync service) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void removedService(ServiceReference<ITimezoneService> reference,
-			ITimezoneService service) {
+	public void removedService(
+			ServiceReference<ITimezoneServiceAsync> reference,
+			ITimezoneServiceAsync service) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
